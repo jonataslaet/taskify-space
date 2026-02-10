75a99c9635a91048ff33a19c2c11574c18027050
jonataslaet <jonataslaetprogramador@gmail.com>
2026-02-10 17:58:39 -0300

    Started project

---
+++ backend-java/src/main/java/com/jonataslaet/taskifyspace/TaskifySpaceApplication.java
@@ -0,0 +1,13 @@
+package com.jonataslaet.taskifyspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifySpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskifySpaceApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/jonataslaet/taskifyspace/TaskifySpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonataslaet.taskifyspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifySpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
